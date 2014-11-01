92569a98a8b1ab070146d3accd40d9f1f4edeb1f
CodyP23 <hiimcodas@gmail.com>
2014-11-01 10:24:01 -0700

    initial stuff
    added account backend files (did not delete existing member and
    employee)
    Cannot update existing accounts, need units.

---
+++ packages/users/server/routes/accounts.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// The Package is past automatically as first parameter
+module.exports = function(Account, app, auth, database) {
+
+var account = require('../controllers/accounts');
+
+//Authorization helper
+var hasAuthorization = function(req, res, next) {
+  if (!req.user.isAdmin && req.account.userID.id !== req.user.id) { //Not sure about syntax on second condition
+    return res.send(401, 'User is not authorized');
+  }
+  next();
+};
+
+module.exports = function(Accounts, app, auth) {
+
+  app.route('/accounts') //Basic listing for employees Need to require authorization for this one
+    .get(accounts.all)	 //No limit on number? Handled elsewhere? No authorization for now...
+	
+  app.route('/account/:articleId')
+    .get(account.show)
+    .put(auth.requiresLogin, hasAuthorization, account.update)
+
+
+  // Finish with setting up the articleId param
+  app.param('accountId', accounts.account);
+};
+
+ };
