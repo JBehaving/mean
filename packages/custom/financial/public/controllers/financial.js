f0231bdcdf28d3895abf30e990b4e2480407cd57
dan <daniel.r.gallegos@gmail.com>
2014-11-15 12:55:59 -0800

    minor updates

--- packages/custom/financial/public/controllers/financial.js
+++ packages/custom/financial/public/controllers/financial.js
@@ -1,17 +1,27 @@
 'use strict';
 
-angular.module('mean.financial').controller('FinancialController', ['$scope', 'Global', 'Financial',
-  function($scope, Global, Financial) {
+angular.module('mean.financial').controller('FinancialController', ['$scope', 'Global', 'Financial', 'Events',
+  function($scope, Global, Financial, Events) {
     $scope.global = Global;
     $scope.package = {
       name: 'financial'
     };
 
-    $scope.test_csv = [{
-        eventDate : '11/11/2014',
-        eventPrice : 150,
-        eventAttendees : 45,
-        eventRevenue : 150 * 45
-    }];
+    $scope.test_csv = [];
+    $scope.years = [];
+    $scope.events = [];
+
+    $scope.init = function() {
+        Events.query(function(events) {
+            $scope.events = events;
+            console.log($scope.events);
+            for (var i = 0; i < events.length; i++) {
+
+            }
+        });
+
+    };
+
+
   }
 ]);
