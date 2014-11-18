aa5877e26297ff1c1ab5471654413fe896a36659
dan <daniel.r.gallegos@gmail.com>
2014-11-17 16:36:15 -0800

    Updated vehicles code, CRUD operations should all be working

--- packages/custom/vehicles/server/controllers/vehicles.js
+++ packages/custom/vehicles/server/controllers/vehicles.js
@@ -1,6 +1,8 @@
+'use strict';
+
 var mongoose = require('mongoose'),
-    Vehicle = mongoose.Model('Vehicle'),
-    ObjectId = mongoose.Types.ObjectId;
+    Vehicle = mongoose.model('Vehicle'),
+    _ = require('lodash');
 
 
 exports.all = function (req,res) {
@@ -18,10 +20,13 @@ exports.all = function (req,res) {
 
 
 exports.show = function(req,res) {
-    if (req.vehicle)
+    console.log(req.vehicle);
+    if (req.vehicle) {
         res.status(200).json(req.vehicle);
     }
-    else res.status(404);
+    else {
+        res.status(404).json({error : 'Requested vehicle ID not found in database'});
+    }
 };
 
 
@@ -44,19 +49,21 @@ exports.vehicle = function(req,res,next,id) {
         if (err) {
             console.log('Error loading vehicle from database');
             console.log(err);
-            res.status(500).json({error : 'Error loading vehicle from database'})
+            res.status(500).json({error : 'Error loading vehicle from database'});
         }
         if (vehicle) {
             req.vehicle = vehicle;
             next();
         }
-        else res.status(404);
+        else res.status(404).json({error : 'Requested vehicle ID not found in database'});
     });
 };
 
 exports.update = function(req,res) {
     if (req.vehicle) {
-        req.vehicle = req.body;
+        var vehicle = req.vehicle;
+        vehicle = _.extend(vehicle, req.body);
+        console.log(vehicle);
         req.vehicle.save(function (err,vehicle) {
             if (err) {
                 console.log('Error updating vehicle ' + vehicle.id);
@@ -70,4 +77,22 @@ exports.update = function(req,res) {
     }
 
     else res.status(404);
-}
+};
+
+exports.delete = function(req,res) {
+    if (req.vehicle) {
+        req.vehicle.remove(function (err,vehicle) {
+            if (err) {
+                console.log('Error occured deleting vehicle ' + req.vehicle._id);
+                console.log(err);
+                res.status(500).json(err);
+            }
+            if (vehicle) {
+                res.status(200).json(vehicle);
+            }
+            else res.status(404);
+        });
+    }
+
+    else res.status(404);
+};
--- packages/custom/vehicles/server/routes/vehicles.js
+++ packages/custom/vehicles/server/routes/vehicles.js
@@ -3,35 +3,16 @@
 // The Package is past automatically as first parameter
 module.exports = function(Vehicles, app, auth, database) {
 
-  var vehicles = require(../controllers/vehicles);
+  var vehicles = require('../controllers/vehicles');
 
-  app.get('/vehicles/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
-  });
-
-  app.get('/vehicles/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
-  });
-
-  app.get('/vehicles/example/admin', auth.requiresAdmin, function(req, res, next) {
-    res.send('Only users with Admin role can access this');
-  });
-
-  app.get('/vehicles/example/render', function(req, res, next) {
-    Vehicles.render('index', {
-      package: 'vehicles'
-    }, function(err, html) {
-      //Rendering a view from the Package server/views
-      res.send(html);
-    });
-  });
-
-  app.get('/vehicles', vehicles.all)
+  app.route('/vehicles')
+      .get(vehicles.all)
       .post(vehicles.create);
 
   app.route('/vehicles/:vehicleId')
       .get(vehicles.show)
-      .put(vehicles.update);
+      .put(vehicles.update)
+      .delete(vehicles.delete);
 
   app.param('vehicleId', vehicles.vehicle);
 };
