74bea9df7586723d1bdada2cdc9dc50d9d3fef5a
dan <daniel.r.gallegos@gmail.com>
2014-11-18 20:40:19 -0800

    fixed Tracks angular service for real now

--- packages/custom/tracks/public/services/tracks.js
+++ packages/custom/tracks/public/services/tracks.js
@@ -2,7 +2,7 @@
 
 angular.module('mean.tracks').factory('Tracks', ['$resource',
     function($resource) {
-        return $resource ({
+        return $resource ('tracks/:trackId',{
             trackId : '@_id'
         },{
             update: {
