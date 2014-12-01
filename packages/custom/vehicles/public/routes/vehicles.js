8517e1c8206a6a2e6661eac3d2a4591c1d55e1be
Cody Lanier <cody.lanier9@gmail.com>
2014-11-30 22:12:54 -0800

    linked navigation link to garage page and adjusted buttons and page title to be all lowercase

--- packages/custom/vehicles/public/routes/vehicles.js
+++ packages/custom/vehicles/public/routes/vehicles.js
@@ -2,13 +2,14 @@
 
 angular.module('mean.vehicles').config(['$stateProvider',
   function($stateProvider) {
-    $stateProvider.state('vehicles example page', {
-      url: '/vehicles/example',
-      templateUrl: 'vehicles/views/index.html'
-    })
-    .state('create vehicle', {
+    $stateProvider
+        .state('garage page', {
+            url: '/vehicles',
+            templateUrl: 'vehicles/views/index.html'
+        })
+        .state('create vehicle', {
           url: '/vehicles/create',
-          templateUrl: 'vehicles/views/create.html',
+          templateUrl: 'vehicles/views/create.html'
         });
   }
 ]);
